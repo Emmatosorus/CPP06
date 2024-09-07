f6fcbd95fd94885ba13cfe16734bbd9f0e7077b8
Emmatosorus <emma@bouteyres.com>
2024-09-07 12:03:34 +0200

    Add ✨: finished and tested ex00

---
+++ ex00/includes/ScalarConverter.h
@@ -0,0 +1,32 @@
+#ifndef SCALARCONVERTER_H
+#define SCALARCONVERTER_H
+
+#include <iostream>
+#include <stdlib.h>
+#include <limits>
+
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define BLUE "\x1b[34m"
+#define PINK "\x1b[35m"
+#define CYAN "\x1b[36m"
+#define WHITE "\x1b[37m"
+#define BOLD "\x1b[1m"
+#define UNDER "\x1b[4m"
+#define END "\x1b[0m"
+
+class ScalarConverter
+{
+	public:
+		static void	convert(std::string num);
+
+	private:
+		ScalarConverter();
+		ScalarConverter( const ScalarConverter & s );
+		~ScalarConverter();
+
+		ScalarConverter & operator=( ScalarConverter & s );
+};
+
+
+#endif
